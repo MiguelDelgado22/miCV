b93a9b1c745b81a4f62d4c7ced4edcbd9ce90b0a
Miguel Delgado <miguel.delgado@globant.com>
2019-06-26 12:58:41 -0500

    Initial Commit

---
+++ CV_Miguel_Delgado/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CV_Miguel_Delgado
+//
+//  Created by Miguel Angel Delgado Alcantara on 6/26/19.
+//  Copyright © 2019 Miguel Angel Delgado Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CV_Miguel_Delgado/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CV_Miguel_Delgado
+//
+//  Created by Miguel Angel Delgado Alcantara on 6/26/19.
+//  Copyright © 2019 Miguel Angel Delgado Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
